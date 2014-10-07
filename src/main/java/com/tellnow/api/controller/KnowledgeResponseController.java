ffdce8077034b3e858e47bcde08299b59e54625c
Liviu Seniuc <liviu.seniuc@reea.net>
2014-10-07 11:22:19 +0300

    added new lines in knowledge test ("Response was correct. <newline> You earned 3 Appreciation Points" and "Correct answer is: xxx. <newline> You did not earn any Appreciation Points").

--- src/main/java/com/tellnow/api/controller/KnowledgeResponseController.java
+++ src/main/java/com/tellnow/api/controller/KnowledgeResponseController.java
@@ -41,9 +41,9 @@ public class KnowledgeResponseController {
 	
 	private static final long REWARD_POINTS = 3;
 	
-	public static final String MESSAGE_GOOD = "Response was good. You received 3 Appreciation points.";
+	public static final String MESSAGE_GOOD = "Response was good.%nYou received 3 Appreciation points.";
 //	public static final String MESSAGE_WRONG = "Response was incorrect. You did not receive any Appreciation Points.";
-	public static final String MESSAGE_WRONG = "Correct answer was: %s. You did not receive any Appreciation Points.";
+	public static final String MESSAGE_WRONG = "Correct answer is: %s.%nYou did not receive any Appreciation Points.";
 	public static final String MESSAGE_NOQUESTION = "Question does not exists.";
 	
 	@RequestMapping(value = "/api/knowledge/test/profile/{profileId}/submit", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -82,7 +82,7 @@ public class KnowledgeResponseController {
 					}
 					if(good){
 						knowledgeQuestionService.updateRewardPoints(profilePrivateId, topicId, REWARD_POINTS);
-						resp.setMessage(MESSAGE_GOOD);
+						resp.setMessage(String.format(MESSAGE_GOOD));
 					} else {
 						String msg = String.format(MESSAGE_WRONG, formatCorrectAnswers(goodAnswers));
 						resp.setMessage(msg);
